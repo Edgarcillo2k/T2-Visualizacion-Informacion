e4f2654ab42cd6e108953b73b49a308af11b973e
Josué Prendas <josue.prendas8@gmail.com>
2021-10-05 19:12:09 -0600

    Commit Final

--- public/sketch.js
+++ public/sketch.js
@@ -1,24 +1,39 @@
-let song, fft, space_between_lines;
+let song, amp, fft, space_between_lines;
 
 function setup() {
   const canvas = createCanvas(600, 600);
   canvas.parent("music-visualizator");
   colorMode(HSB);
-  filter(BLUR, 100);
   fft = new p5.FFT(0.9, 64);
-  space_between_lines = (width/2) / 64;
+  amp = new p5.Amplitude();
+  space_between_lines = width / 2 / 64;
 }
 
 function draw() {
   background(0);
-  
+
   let spectrum = fft.analyze();
   for (let i = 0; i < spectrum.length; i++) {
-    fill(i*3,255,255); //fill(i,255,255);
-    let amp = spectrum[i];
+    fill(i * 3, 255, 255);
+    let amp = spectrum[i] * 1.6;
     let y = map(amp, 0, 256, height, 350);
-    rect((width/2) + (i * space_between_lines), y, space_between_lines, height - y);
-    rect((width/2) - (i * space_between_lines), y, space_between_lines, height - y);
+    rect(
+      width / 2 + i * space_between_lines,
+      y / 2,
+      space_between_lines,
+      height - y
+    );
+    rect(
+      width / 2 - i * space_between_lines,
+      y / 2,
+      space_between_lines,
+      height - y
+    );
+    push();
+    fill(0);
+    strokeWeight(3);
+    line(0, height / 2, width, height / 2);
+    pop();
   }
 }
 
